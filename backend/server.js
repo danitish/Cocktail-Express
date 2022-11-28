1d7d453c89f3b74e7c0e0f598a15edbec92f6ab2
danitish <danitishenko23@gmail.com>
2022-11-28 19:11:05 +0200

    test

--- backend/server.js
+++ backend/server.js
@@ -30,9 +30,11 @@ app.use("/api/items", itemRouter);
 app.use("/api/menu_items", menuItemRouter);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+    res.sendFile(
+      path.resolve(__dirname, "..", "frontend", "build", "index.html")
+    );
   });
 }
 
