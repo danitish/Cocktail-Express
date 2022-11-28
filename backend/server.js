8d93f2423a2ab3a7988f1e65921f5cea0b159654
danitish <danitishenko23@gmail.com>
2022-11-28 19:06:01 +0200

    deploy test

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const path = require("path");
 
 const connectDB = require("./config/db");
 const { notFound, errorHandler } = require("./middleware/errors");
